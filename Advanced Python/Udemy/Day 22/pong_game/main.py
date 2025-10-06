3dd4fb8e1d1bb589b417242c42077e66321a8cd3
Kibet Sang <kibetsang95@gmail.com>
2025-10-06 10:41:31 +0300

    feat: turtle car game

--- Advanced Python/Udemy/Day 22/pong_game/main.py
+++ Advanced Python/Udemy/Day 22/pong_game/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
@@ -9,7 +11,8 @@ screen.tracer(0)
 game_is_on = True
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
-
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.paddle_up, "Up")
@@ -17,7 +20,22 @@ screen.onkey(r_paddle.paddle_down, "Down")
 screen.onkey(l_paddle.paddle_up, "w")
 screen.onkey(l_paddle.paddle_down, "s")
 while game_is_on:
+    time.sleep(ball.move_speed)
+    ball.move()
     screen.update()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
 
 
 
---
+++ Advanced Python/Udemy/Day 23/turtle-crossing/main.py
@@ -0,0 +1,30 @@
+import time
+from turtle import Screen
+from zipapp import create_archive
+
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("white")
+screen.tracer(0)
+
+
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+screen.onkey(player.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+screen.exitonclick()
