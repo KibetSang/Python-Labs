9e97925af100cc4eebf793f96064c2f0243754c4
Kibet Sang <kibetsang95@gmail.com>
2025-10-25 21:05:41 +0300

    feat: turtle car

--- Advanced Python/Udemy/Day 23/turtle-crossing-start/main.py
+++ Advanced Python/Udemy/Day 23/turtle-crossing-start/main.py
@@ -1,30 +1,46 @@
 import time
+import turtle
 from turtle import Screen
-from zipapp import create_archive
-
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("white")
-screen.tracer(0)
 
+screen = Screen()
+screen.setup(width=500, height=500)
+screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
-screen.onkey(player.go_down, "Down")
+
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.3)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+
+
+
+
+
 
 
 screen.exitonclick()
