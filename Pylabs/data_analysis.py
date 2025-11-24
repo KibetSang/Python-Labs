d1229729647a1df8f45d99d56247d3ad93e252cb
Kibet Sang <kibetsang95@gmail.com>
2025-11-24 11:12:00 +0300

    feat: Classes and Dict

--- Pylabs/data_analysis.py
+++ Pylabs/data_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 data = pd.read_csv("E:/Training Materials/Pylabs\data.csv")
 print(data.loc[data['ID'] == 1, 'Grade'].item())
 
+
 # print(data.Grade[data.ID == 1].item())
 # print(data.loc[data['ID'] == 1, ['Grade', 'Math']])
 
